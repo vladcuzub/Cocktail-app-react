0fb306a29c87835647c9eb2942e0a3152e9d7696
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2023-02-09 20:38:50 +0100

    Updates

---
+++ precache-manifest.52c01309a9245f6785f66f85a62dbcbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c153806068ee9c53ae2",
+    "url": "/Cocktail-app-react/static/css/main.09575130.chunk.css"
+  },
+  {
+    "revision": "7c153806068ee9c53ae2",
+    "url": "/Cocktail-app-react/static/js/main.407b7623.chunk.js"
+  },
+  {
+    "revision": "099d6660c59e16b871ed",
+    "url": "/Cocktail-app-react/static/js/runtime~main.b23ac37b.js"
+  },
+  {
+    "revision": "9b1b9a024b320f703a3d",
+    "url": "/Cocktail-app-react/static/js/2.4ff45e50.chunk.js"
+  },
+  {
+    "revision": "5c121506b54d36665e4f60dfb5ff5c09",
+    "url": "/Cocktail-app-react/static/media/logo.5c121506.svg"
+  },
+  {
+    "revision": "349db1fb2047415b5f579276801eddfb",
+    "url": "/Cocktail-app-react/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Cocktail-app-react/precache-manifest.52c01309a9245f6785f66f85a62dbcbe.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Cocktail-app-react/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
